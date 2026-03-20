a9ac147b1a160d8f549bd2c06e614ee660348242
sanoyuji1007 <sanoyuji1007@gmail.com>
2026-03-21 07:19:59 +0900

    ModId「sword-add」→「swordadd」

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "sword-add";
+	public static final String MOD_ID = "swordadd";
 
 	// ログを出力し、MODが動いてるか確認する
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
