d3803879212a33582a26b26c35dda77a1f818547
sanoyuji1007 <sanoyuji1007@gmail.com>
2026-03-21 04:44:19 +0900

    アイテム登録クラス作成

---
+++ src/main/java/com/example/ModItems.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    // アイテムを追加
+    public static final Item BLACK_SWORD = register("black_sword", Item::new, new Item.Settings());
+
+    // Item オブジェクトを返す register メソッド
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+
+        // アイテムキーを作成
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TemplateMod.MOD_ID, name));
+
+        // アイテムインスタンスを作成
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // アイテムを登録
+        Registry.register(Registries.ITEM, itemKey, item);
+        return item;
+    }
+
+    public static void initialize() {
+
+        //　ログに出力して、Modが正しく初期化されたことを確認する
+        TemplateMod.LOGGER.info("MODアイテムの登録 " + TemplateMod.MOD_ID);
+
+        // クリエイティブタブに追加（FOOD_AND_DRINKが「食べ物を飲み物」のタブ)
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
+                .register(entries -> {
+                    entries.add(BLACK_SWORD); // BLACK_SWORDを追加します
+                });
+    }
+
+}
--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -1,24 +1,17 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TemplateMod implements ModInitializer {
 	public static final String MOD_ID = "sword-add";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	// ログを出力し、MODが動いてるか確認する
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.initialize(); // ← これを呼ぶことで、ModItemsクラスをロード
 	}
 }
