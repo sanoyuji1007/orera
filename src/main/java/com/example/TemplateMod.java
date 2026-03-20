38327672b442cf179d9f899992cf05c5c8216c94
sanoyuji1007 <sanoyuji1007@gmail.com>
2026-03-21 03:06:49 +0900

    iconの編集

--- src/main/java/com/example/TemplateMod.java
+++ src/main/java/com/example/TemplateMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "template-mod";
+	public static final String MOD_ID = "sword-add";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
