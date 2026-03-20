6d6c757ab617c7f5c2f24724e6b8682ca63cccc2
sanoyuji1007 <sanoyuji1007@gmail.com>
2026-03-21 06:14:28 +0900

    黒い剣の素材と性能を追加

--- src/main/java/com/example/ModItems.java
+++ src/main/java/com/example/ModItems.java
@@ -3,18 +3,37 @@ package com.example;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
 
 public class ModItems {
 
-    // アイテムを追加
-    public static final Item BLACK_SWORD = register("black_sword", Item::new, new Item.Settings());
+    //黒い剣の素材の性能
+    public static final ToolMaterial BLACK_SWORD_MATERIAL = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_NETHERITE_TOOL,
+            2500,
+            9.0F,
+            8.0F,
+            30,
+            ItemTags.NETHERITE_TOOL_MATERIALS
+    );
+
+
+    // 黒い剣を追加
+    public static final Item BLACK_SWORD = register(
+            "black_sword",
+            Item::new, // 第2引数：itemFactory (Itemクラスのコンストラクタを渡す)
+            new Item.Settings()
+                    .sword(BLACK_SWORD_MATERIAL, 6.0F, -2.4F)
+    );
 
     // Item オブジェクトを返す register メソッド
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
